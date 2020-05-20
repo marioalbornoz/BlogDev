543fcd64adb4b164f5d0ca6e7a490fe35ac7a15b
marioalbornoz <mario.albornozc@utem.cl>
2020-05-20 01:06:00 -0400

    initial commit

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css'
+
+import CustomLayout from './containers/Layout';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
