20dcd63f3f5f9afa07b4baffe2af595da4d89c36
marioalbornoz <mario.albornozc@utem.cl>
2020-05-20 18:56:30 -0400

    fix menores

--- frontend/gui/src/containers/ArticleListView.js
+++ frontend/gui/src/containers/ArticleListView.js
@@ -2,19 +2,6 @@ import React from 'react';
 import axios from 'axios';
 import Articles from '../components/Article';
 
-const listData = [];
-for (let i = 0; i < 23; i++) {
-    listData.push({
-        href: 'https://ant.design',
-        title: `ant design part ${i}`,
-        avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        description:
-            'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-        content:
-            'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-    });
-}
-
 class ArticleList extends React.Component {
 
     state = {
@@ -27,7 +14,6 @@ class ArticleList extends React.Component {
                 this.setState({
                     articles : res.data
                 })
-                console.log(res.data);
                 
             })
     }
