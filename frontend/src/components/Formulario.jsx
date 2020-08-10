209e91d9a4ae1b2e19b24b9875e101ef9db407a3
marioalbornoz <36382828+marioalbornoz@users.noreply.github.com>
2020-08-10 19:35:38 -0400

    Se agrega edit al proyecto

--- frontend/src/components/Formulario.jsx
+++ frontend/src/components/Formulario.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, {  } from 'react';
 
 const API = process.env.REACT_APP_API;
 
 
-const Formulario = ({guardarPosts}) => {
-  const [titulo, guardarTitulo] = useState("");
-  const [texto, guardarTexto] = useState("");
-
+const Formulario = ({
+  guardarPosts,
+  titulo,
+  texto,
+  guardarTitulo,
+  guardarTexto,
+  editando,
+  idpost,
+}) => {
   const inputTitulo = (e) => {
     guardarTitulo(e.target.value);
   };
@@ -14,58 +19,75 @@ const Formulario = ({guardarPosts}) => {
     guardarTexto(e.target.value);
   };
   const getPost = async () => {
-    const res = await fetch(`http://127.0.0.1:8000/api/`);
+    const res = await fetch(`${API}`);
     const response = await res.json();
     console.log(response);
     guardarPosts(response);
   };
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch(`${API}`, {
-      method: "POST",
-      headers:{
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify({
-        title:titulo,
-        content: texto,
-      }),
-    });
-    const data = await res.json();
-    console.log('====================================');
-    console.log(data);
-    console.log('====================================');
+    if(editando === false){
+      const res = await fetch(`${API}`, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          title: titulo,
+          content: texto,
+        }),
+      });
+      const data = await res.json();
+      
+      console.log(data);
+    }
+    else {
+      const res = await fetch(`${API}${idpost}/`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+          title: titulo,
+          content: texto,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+    }
+    
     getPost();
     guardarTitulo("");
     guardarTexto("");
-  }
+  };
 
- 
   return (
-
-        <form onSubmit={handleSubmit} className="card card-body">
-          <input
-            type="text"
-            name=""
-            onChange={inputTitulo}
-            value={titulo}
-            placeholder="ingrese titulo aqui.."
-            className="form-control"
-            autoFocus
-          />
-          <hr/>
-           <textarea
-            type="text"
-            name=""
-            onChange={inputTexto}
-            value={texto}
-            placeholder="ingrese texto aqui.."
-            className="form-control"
-            >
-            </textarea>
-            <button className="btn btn-primary btn-block">Guardar</button>
-        </form>
+    <form onSubmit={handleSubmit} className="card card-body">
+      <input
+        type="text"
+        name=""
+        onChange={inputTitulo}
+        value={titulo}
+        placeholder="ingrese titulo aqui.."
+        className="form-control"
+        autoFocus
+      />
+      <hr />
+      <textarea
+        type="text"
+        name=""
+        onChange={inputTexto}
+        value={texto}
+        placeholder="ingrese texto aqui.."
+        className="form-control"
+      ></textarea>
+      {
+        editando? <button className="btn btn-danger btn-block">Editar</button>
+        : <button className="btn btn-primary btn-block">Guardar</button>
+      }
+      
+    </form>
   );
 };
  
--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -9,6 +9,10 @@ const API = process.env.REACT_APP_API;
 export const Post = () => {
 
     const [posts, guardarPosts] = useState([]);
+    const [titulo, guardarTitulo] = useState("");
+    const [texto, guardarTexto] = useState("");
+    const [editando, setEditando] = useState(false);
+    const [idpost, setID] = useState('');
 
     useEffect(() => {
       getPost();
@@ -25,24 +29,52 @@ export const Post = () => {
       await fetch(`${API}${id}`, { method: "DELETE" })
       await getPost();
     }
+
+    const handleEdit = async(id) => {
+      const res = await fetch(`${API}${id}`)
+      const data = await res.json();
+
+      // State que es usado al momento de editar
+      setEditando(true);
+      setID(id)
+      
+      // Se llena el formulario con los datos tomados
+      guardarTitulo(data.title);
+      guardarTexto(data.content)
+    }
     
     return (
       <Fragment>
         <div className="row">
           <div className="col col-4">
-            <Formulario guardarPosts = {guardarPosts}/>
+            <Formulario
+              titulo={titulo}
+              guardarTitulo={guardarTitulo}
+              texto ={texto}
+              guardarTexto = {guardarTexto}
+              guardarPosts={guardarPosts}
+              editando = {editando}
+              idpost = {idpost}
+            />
           </div>
           <div className="col col-8 posteos">
             {posts.map((post) => (
               <div className="card w-45 m-2" key={post.id}>
                 <div className="card-header">
                   <h5>{post.title}</h5>{" "}
-                  <button type="submit" onClick={()=>handleDelete(post.id)} className="btn btn-ligth">
+                  <button
+                    type="submit"
+                    onClick={() => handleDelete(post.id)}
+                    className="btn btn-ligth"
+                  >
                     x
                   </button>
                 </div>
-                <div className="card-body">
-                <p>{post.content}</p>
+                <div
+                  className="card-body"
+                  onDoubleClick={() => handleEdit(post.id)}
+                >
+                  <p>{post.content}</p>
                 </div>
               </div>
             ))}
